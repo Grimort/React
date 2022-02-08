1a4c23f34ee2fdd9e33ee04aead8379e2b7e7b94
Sebastian <sebastian.ospina@system73.com>
2022-02-08 14:48:51 +0000

    adding ignore

--- 04-gif-app/src/tests/hooks/useFetchGifs.test.js
+++ 04-gif-app/src/tests/hooks/useFetchGifs.test.js
@@ -1,9 +1,12 @@
-import { renderHook } from "@testing-library/react-hooks";
 import { useFetchGifs } from "../../hooks/useFetchGifs";
+import { renderHook } from "@testing-library/react-hooks";
 
 describe("Testing useFetchGifs hook", () => {
   test("should return the initial state", () => {
     // const { data, loading } = useFetchGifs("Memes");
-    renderHook(useFetchGifs("Memes"));
+    const { result } = renderHook(() => useFetchGifs("Memes"));
+    console.log(result);
+    const { data, loading } = result.current;
+    console.log(data, loading);
   });
 });
